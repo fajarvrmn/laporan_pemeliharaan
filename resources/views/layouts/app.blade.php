7fa0fa9a965bf11b1241379fccb368dc9d14ef15
Nk121212 <setiawankahfii@gmail.com>
2023-07-09 00:36:16 +0700

    perbaikan layout logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,7 +112,7 @@
         <nav class="main-header navbar navbar-expand navbar-dark">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
@@ -120,17 +120,6 @@
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="#" class="nav-link">Contact</a>
-                </li>
-                <!-- <li class="nav-item d-none d-sm-inline-block">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
                 </li> -->
                 
             </ul>
@@ -138,7 +127,7 @@
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
                 <!-- Navbar Search -->
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link" data-widget="navbar-search" href="#" role="button">
                     <i class="fas fa-search"></i>
                     </a>
@@ -157,11 +146,26 @@
                         </div>
                     </form>
                     </div>
-                </li>
-                <li class="nav-item">
+                </li> -->
+                <!-- <li class="nav-item">
                     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                    <i class="fas fa-expand-arrows-alt"></i>
+                        <i class="fas fa-expand-arrows-alt"></i>
                     </a>
+                </li> -->
+                <li class="nav-item">
+                    <!-- <a class="nav-link" href="#" role="button">
+                        <i class="fas fa-sign-out-alt"></i>
+                    </a> -->
+                    <a class="nav-link" role="button" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i>
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </li>
             </ul>
         </nav>
