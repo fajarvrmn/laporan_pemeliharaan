9132c8cef5b365b49fd82cc10047c3121d01d62c
Nk121212 <setiawankahfii@gmail.com>
2023-07-19 14:25:14 +0700

    perbaikan login role

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="{{ route('home') }}" class="brand-link">
+            <a href="{{ route('home.index') }}" class="brand-link">
             <img src="{{ asset('theme/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,56 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware(['admin'])->group(function () {
-
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-    Route::prefix('admin')->group(function () {
-
-        // Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-        Route::resource('dashboard', App\Http\Controllers\DashboardController::class);
-        Route::resource('merekperalatan', App\Http\Controllers\MerekController::class);
-        Route::resource('tipeperalatan', App\Http\Controllers\TipeController::class);
-        Route::resource('garduinduk', App\Http\Controllers\GarduController::class);
-        Route::resource('status', App\Http\Controllers\StatusController::class);
-        Route::resource('peralatan', App\Http\Controllers\PeralatanController::class);
-        Route::resource('personil', App\Http\Controllers\PersonilController::class);
-        Route::resource('laporan', App\Http\Controllers\LaporanController::class);
-        // Route::resource('namabay', NamabayController::class);
-
-    });
-
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('home', App\Http\Controllers\HomeController::class);
+    Route::resource('dashboard', App\Http\Controllers\DashboardController::class);
+    Route::resource('merekperalatan', App\Http\Controllers\MerekController::class);
+    Route::resource('tipeperalatan', App\Http\Controllers\TipeController::class);
+    Route::resource('garduinduk', App\Http\Controllers\GarduController::class);
+    Route::resource('status', App\Http\Controllers\StatusController::class);
+    Route::resource('peralatan', App\Http\Controllers\PeralatanController::class);
+    Route::resource('personil', App\Http\Controllers\PersonilController::class);
+    Route::resource('laporan', App\Http\Controllers\LaporanController::class);
 });
+
+// Route::middleware(['admin'])->group(function () {
+
+//     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//     Route::prefix('admin')->group(function () {
+
+//         // Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//         Route::resource('dashboard', App\Http\Controllers\DashboardController::class);
+//         Route::resource('merekperalatan', App\Http\Controllers\MerekController::class);
+//         Route::resource('tipeperalatan', App\Http\Controllers\TipeController::class);
+//         Route::resource('garduinduk', App\Http\Controllers\GarduController::class);
+//         Route::resource('status', App\Http\Controllers\StatusController::class);
+//         Route::resource('peralatan', App\Http\Controllers\PeralatanController::class);
+//         Route::resource('personil', App\Http\Controllers\PersonilController::class);
+//         Route::resource('laporan', App\Http\Controllers\LaporanController::class);
+//         // Route::resource('namabay', NamabayController::class);
+
+//     });
+
+// });
+
+// Route::middleware(['supervisor'])->group(function () {
+
+//     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//     Route::prefix('supervisor')->group(function () {
+
+//         // Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//         Route::resource('dashboard', App\Http\Controllers\DashboardController::class);
+//         Route::resource('merekperalatan', App\Http\Controllers\MerekController::class);
+//         Route::resource('tipeperalatan', App\Http\Controllers\TipeController::class);
+//         Route::resource('garduinduk', App\Http\Controllers\GarduController::class);
+//         Route::resource('status', App\Http\Controllers\StatusController::class);
+//         Route::resource('peralatan', App\Http\Controllers\PeralatanController::class);
+//         Route::resource('personil', App\Http\Controllers\PersonilController::class);
+//         Route::resource('laporan', App\Http\Controllers\LaporanController::class);
+//         // Route::resource('namabay', NamabayController::class);
+
+//     });
+
+// });
